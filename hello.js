3ca064c88a8b2e8d296e57a1a97873bb1ad41313
corynezin <cory.nezin@gmail.com>
2019-01-28 00:25:55 -0500

    style upgrade

--- hello.js
+++ hello.js
@@ -1,12 +1,19 @@
+window.onload = function() {
+    text = document.getElementById('topText').innerHTML
+    typewrite(text)
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function typewrite(i, text, time) {
+async function typewrite(text) {
     var writtenText = ''
     // Loop over characters, adding one by one
+    var i = 0
     for (i = 0; i < text.length; i++) {
         writtenText += text.charAt(i)
+        console.log(writtenText)
         document.getElementById('topText').innerHTML = writtenText + '|'
         sleeptime = 300 - (Math.random()*100 + Math.random()*100 + Math.random()*100)
         console.log(sleeptime)
