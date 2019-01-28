ad2f6bf106fc07e8c284bc736f7ec78ec301eeba
corynezin <cory.nezin@gmail.com>
2019-01-27 23:58:44 -0500

    add better typewrite

--- hello.js
+++ hello.js
@@ -1,17 +1,23 @@
-function typewrite(i, text, time) {
-    if (text === undefined) {
-        text = ''
-    }
-    // Get text already in body
-    // text = document.getElementById('topText').innerHTML
-    // Set to empty string to stop display
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function typewrite(i, text, time) {
+    var writtenText = ''
     // Loop over characters, adding one by one
-    if (i < text.length) {
-        console.log(i)
-        console.log(text.charAt(i))
-        document.getElementById('topText').innerHTML += text.charAt(i)
-        i++
-        setTimeout(function() {typewrite(i, text, time); }, time)
+    for (i = 0; i < text.length; i++) {
+        writtenText += text.charAt(i)
+        document.getElementById('topText').innerHTML = writtenText + '|'
+        sleeptime = 300 - (Math.random()*100 + Math.random()*100 + Math.random()*100)
+        console.log(sleeptime)
+        await sleep(sleeptime)
+    }
+    await sleep(1000)
+    while (true) {
+        document.getElementById('topText').innerHTML = writtenText + ''
+        await sleep(1000)
+        document.getElementById('topText').innerHTML = writtenText + '|'
+        await sleep(1000)
     }
 }
 
