a2ceff237497e19351f4d8abe3785f99b6089aa8
corynezin <cory.nezin@gmail.com>
2019-01-27 23:22:57 -0500

    add typewriter function

--- hello.js
+++ hello.js
@@ -1,3 +1,19 @@
+function typewrite(i, text, time) {
+    if (text === undefined) {
+        text = ''
+    }
+    // Get text already in body
+    // text = document.getElementById('topText').innerHTML
+    // Set to empty string to stop display
+    // Loop over characters, adding one by one
+    if (i < text.length) {
+        console.log(i)
+        console.log(text.charAt(i))
+        document.getElementById('topText').innerHTML += text.charAt(i)
+        i++
+        setTimeout(function() {typewrite(i, text, time); }, time)
+    }
+}
 
 function changeTitle() {
     document.getElementById('topText').innerHTML='Deborah Gorelik'
